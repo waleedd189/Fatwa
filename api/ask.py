f2f260cf0517ca5f7bfef7e119c207792f5a7d5f
waleedd189 <waleedd2004@gmail.com>
2026-07-11 01:10:49 +0300

    Add files via upload

---
+++ api/ask.py
@@ -0,0 +1,16 @@
+import json
+
+def handler(request):
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*"
+        },
+        "body": json.dumps({
+            "status": "ok",
+            "message": "API test successful"
+        })
+    }
+
+app = handler
